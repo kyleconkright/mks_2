12a7db827076c3add98ce3a351a4307ce5fc0f7b
kyleconkright <kyleconkright@gmail.com>
2015-05-16 15:04:16 -0700

    created controllers for users and orders

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :street1, :street2, :city, :state, :zip
 
-  has_many :orders, :serializer => ShortOrderSerializer
+  # has_many :orders, :serializer => ShortOrderSerializer
 
 end
