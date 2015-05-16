51a8f10f811483bb24e65e5df41435c0b0380fef
Mike Pacheco <mikelnpacheco@gmail.com>
2015-05-16 15:56:42 -0700

    order model test

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-  "should have QTY"
-  "should have status"
-  "should have size"
+	it "should have QTY"  do
+	order = FactoryGirl.build(:order, quantity: 1)
+	expect(order.quantity).to eq(1)
+	end 
+	it "should have status"  do
+	order = FactoryGirl.build(:order, status: "pending")
+	expect(order.status).to eq("pending")
+	end 
+	it"should have size" do 
+	order = FactoryGirl.build(:order, size: "small")
+	expect(order.size).to eq("small")
+	end 
 end
