68b786d242d538c1747f0472bdbeef019a1cf02a
kyleconkright <kyleconkright@gmail.com>
2015-05-16 14:43:58 -0700

    added orders to users json

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :street1, :street2, :city, :state, :zip
 
-  has_many :orders, :embed => :ids
-  
+  has_many :orders, :serializer => ShortOrderSerializer
+
 end
