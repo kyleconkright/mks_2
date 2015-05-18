950556ad968c1f81cd41758b74a473f043f327f9
Mike Pacheco <mikelnpacheco@gmail.com>
2015-05-17 18:51:57 -0700

    added testing to address model

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+ it "should have a street1" do 
+ 	 	address = FactoryGirl.build(:address, street1: "123 main street")
+ 	 	expect(address.street1).to eq("123 main street")
+ 	 end
+ 	 it "should have a street2"do 
+ 	 	address = FactoryGirl.build(:address, street2: "123 main street")
+ 	 	expect(address.street2).to eq("123 main street")
+ 	 end
+ 	 it "should have a city" do 
+ 	 	address = FactoryGirl.build(:address, city: "Los Angeles")
+ 	 	expect(address.city).to eq("Los Angeles")
+ 	 end
+ 	 it "should have a state" do 
+ 	 	address = FactoryGirl.build(:address, state: "CA")
+ 	 	expect(address.state).to eq("CA")
+ 	 end
+
+ 	 it "should have a zip" do
+ 	 	address = FactoryGirl.build(:address, zip: 90034)
+ 	 	expect(address.zip).to eq(90034)
+ 	 end
+ end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,27 +13,7 @@ RSpec.describe User, type: :model do
  	#  	user = FactoryGirl.build(:user, password: "password")
 		# expect(user.password).to eq("password")
  	#  end
- 	 it "should have a street1" do 
- 	 	user = FactoryGirl.build(:user, street1: "123 main street")
- 	 	expect(user.street1).to eq("123 main street")
- 	 end
- 	 it "should have a street2"do 
- 	 	user = FactoryGirl.build(:user, street2: "123 main street")
- 	 	expect(user.street2).to eq("123 main street")
- 	 end
- 	 it "should have a city" do 
- 	 	user = FactoryGirl.build(:user, city: "Los Angeles")
- 	 	expect(user.city).to eq("Los Angeles")
- 	 end
- 	 it "should have a state" do 
- 	 	user = FactoryGirl.build(:user, state: "CA")
- 	 	expect(user.state).to eq("CA")
- 	 end
-
- 	 it "should have a zip" do
- 	 	user = FactoryGirl.build(:user, zip: 90034)
- 	 	expect(user.zip).to eq(90034)
- 	 end
+ 	 
 end
 
   
